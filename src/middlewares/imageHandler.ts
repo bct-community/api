2e38d676087e02869f085e1085a746bb3c1189b5
BCT Community <bctcommunity01@gmail.com>
2025-01-31 23:30:14 -0300

    feat: add image compression using sharp in imageHandler middleware

--- src/middlewares/imageHandler.ts
+++ src/middlewares/imageHandler.ts
@@ -1,6 +1,7 @@
 import { endResponseWithCode } from '@/utils/http.js';
 import type { Request, Response, NextFunction } from 'express';
 import multer from 'multer';
+import sharp from 'sharp';
 
 const storage = multer.memoryStorage();
 
@@ -16,11 +17,27 @@ const upload = multer({
   },
 }).single('image');
 
-const imageHandler = (_req: Request, res: Response, next: NextFunction) => {
-  upload(_req, res, function (err) {
-    if (err) endResponseWithCode(res, 400);
+const imageHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  upload(req, res, async function (err) {
+    if (err) return endResponseWithCode(res, 400);
 
-    next();
+    if (!req.file) return endResponseWithCode(res, 400);
+
+    try {
+      const compressedImage = await sharp(req.file.buffer)
+        .resize(800)
+        .jpeg({ quality: 80 })
+        .toBuffer();
+
+      req.file.buffer = compressedImage;
+      next();
+    } catch (error) {
+      return endResponseWithCode(res, 500);
+    }
   });
 };
 
